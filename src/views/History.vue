55ed3dd93fe9ee13608c010adf0269cfe8464d6b
Quang Hung <ngochung.quang.1310@gmail.com>
2020-07-03 16:39:48 +0700

    Update history view

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      <div class="md-layout-item md-medium-size-100 md-size-66">
-        <edit-profile-form data-background-color="green"> </edit-profile-form>
-      </div>
-      <div class="md-layout-item md-medium-size-100 md-size-33">
-        <user-card> </user-card>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card>
+          <md-card-header data-background-color="blue">
+            <h4 class="title">Lịch sử</h4>
+          </md-card-header>
+          <md-card-content>
+            <history-table table-header-color="green"></history-table>
+          </md-card-content>
+        </md-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { HistoryTable } from "@/components";
 
 export default {
   components: {
+    HistoryTable
   }
 };
 </script>
